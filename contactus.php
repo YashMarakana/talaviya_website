7587cb0fc967ce6f75920c35c90503edcc5caf16
Yash Marakana <68960729+YashMarakana@users.noreply.github.com>
2021-07-08 22:41:50 +0530

    talaviya impex website

---
+++ contactus.php
@@ -0,0 +1,401 @@
+<?php
+if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$phone=$_POST['phone'];
+		$msg=$_POST['message'];
+
+		$to='talaviyaimpex5@gmail.com'; // Receiver Email ID, Replace with your email ID
+		
+		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
+
+		if(mail($to, $msg, $message, $headers)){
+			 
+		}
+		else{
+			 
+                         echo "Done";
+		}
+	}
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+ 
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <meta name="google-site-verification" content="1DIjHdgSqNNjrenp53tKA5ui39TYa9vlGez2pQ8awH4" />
+    <meta name="google-site-verification" content="1DIjHdgSqNNjrenp53tKA5ui39TYa9vlGez2pQ8awH4" /> -->
+  <meta name="robots" content="all, index, follow" />
+  <meta name="revisit-after" content="7 days" />
+  <title>Talaviya Impex business in India | import and export in India</title>
+  <meta name="description"
+    content="We are one of the best imports and export great business in India engaged in exporting quality products to clients at best rates." />
+  <meta name="keywords"
+    content="How to start export business from home, export great business in India without investment, major imports of India 2020, list of products exported from India. " />
+  <meta name="news_keywords"
+    content="How to start export business from home, export great business in India without investment, major imports of India 2020, list of products exported from India. " />
+  <meta property="og:title" content="Import export great business in India | import and export in India" />
+  <meta property="og:description"
+    content="We are one of the best imports and export great business in India engaged in exporting quality products to clients at best rates." />
+  <meta property="og:image" content="images/logo-tp.png" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content=" http://www.talaviyaimpex.com" />
+  <link rel="canonical" href="index.html" />
+  <meta name="DC.title" content="Import export great business in India | import and export in India" />
+  <meta name="geo.region" content="IN-TN" />
+  <meta name="geo.placename" content="Gujarat" />
+  <!-- <meta name="geo.position" content="13.10859;80.245129" />
+    <meta name="ICBM" content="13.10859, 80.245129" /> -->
+  <meta name="copyright" content="Copyrights @ 2020 Talaviya Impex. All rights Reserved." />
+  <meta name="country" content="India" />
+  <meta name="geography" content="India" />
+  <meta name="language" content="en-us" />
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+  <link rel="shortcut icon" href="images/favi.png" type="text/css" />
+  <link rel="stylesheet" type="text/css"
+    href="../maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="engine1/style.css" />
+  <link rel="stylesheet" href="css/menustyle.css">
+  <link rel="stylesheet" href="css/jflex.min.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+</head>
+
+<body>
+  <div class="top">
+    <div class="container">
+      <div class="col-md-4 logo">
+        <a href="index.html"><img src="images/logo-tp.png" alt="import and export business" title="" /></a>
+      </div>
+
+      <!-- <div class="col-md-5">
+        <p>+(91)-98408 00004 | <i class="fa fa-envelope"></i> <a href="mailto:durgaprasad@kankasexports.com">durgaprasad@kankasexports.com</a></p></div> -->
+
+      <div class="col-md-2">
+        <div id="google_translate_element"></div>
+      </div>
+
+      <div class="col-md-7">
+        <a class="toggleMenu" href="#">Menu</a>
+        <ul class="nav">
+          <li class="test">
+            <a href="index.html">Home</a>
+          </li>
+          <li><a href="aboutus.html">About Us</a></li>
+
+          <li><a class="active" href="#">Products</a>
+            <ul>
+              <li><a href="#">Parishable Item</a>
+                <ul>
+                  <li><a href="herbs-and-spices-exporters-in-india.html">Mango</a></li>
+                  <li><a href="dry-fruits-importers-exporters-in-india.html">Pomegranates</a></li>
+                  <li><a href="best-export-company-for-nuts-and-seeds.html">Onion</a></li>
+                  <li><a href="fresh-fruits-exporting-company.html">Fresh Fruits</a></li>
+                  <li><a href="vegetables-exporter-in-india.html">Fresh Vegetables</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Non Parishable Item</a>
+                <ul>
+                  <li><a href="herbs-and-spices-exporters-in-india.html">Mango Pulp</a></li>
+                  <li><a href="#">Pickles</a></li>
+                  <li><a href="honey-exporters-in-india.html">Processed Food Items</a></li>
+                </ul>
+              </li>
+              <li><a href="product-enquiry.html">Scrap</a>
+                <!-- <ul>
+                  <li><a href="herbs-and-spices-exporters-in-india.html">Aluminium Scrap</a></li>
+                  <li><a href="herbs-and-spices-exporters-in-india.html">OCC-11</a></li>
+                  <li><a href="herbs-and-spices-exporters-in-india.html">OCC-12</a></li>
+                  <li><a href="herbs-and-spices-exporters-in-india.html">Cast iron Scrap</a></li>
+                </ul> -->
+              </li>
+            </ul>
+          </li>
+
+
+          <li><a href="#">Tiles</a>
+            <ul>
+              <li><a href="#">Floor Tiles</a>
+                <ul>
+                  <li><a href="glazedvitrified.html">Glazed Vitrified Tiles</a></li>
+                  <li><a href="doublechargetiles.html">Double Charge Tiles</a></li>
+                  <li><a href="solublesalt.html">Soluble Salt (NANO)</a></li>
+                  <li><a href="fullbodytiles.html">Full Body Tiles</a></li>
+                  <li><a href="parkingtiles.html">Parking Tiles</a></li>
+                  <li><a href="conceptfloortiles.html">Concept Floor Tiles</a></li>
+                  <li><a href="stepandrisertiles.html">Step & Riser Tiles</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Wall Tiles</a>
+                <ul>
+                  <li><a href="digitalwalltiles.html">Digital Wall Tiles</a></li>
+                  <li><a href="bathroomtiles.html">Bathroom Wall Tiles</a></li>
+                  <li><a href="kitchentiles.html">Kitchen Wall Tiles</a></li>
+                  <li><a href="elevationwalltiles.html">Elevation Wall Tiles</a></li>
+                  <li><a href="conceptwalltiles.html">Concept Wall Tiles</a></li>
+                </ul>
+              </li>
+              <li><a href="sanitaryware.html">Sanatary Ware</a>
+                <!-- <ul>
+                <li><a href="herbs-and-spices-exporters-in-india.html">Titenia</a></li>
+              </ul> -->
+              </li>
+            </ul>
+          </li>
+          <li><a href="blog-for-import-and-export-companies-india.html">Service</a></li>
+          <li><a href="contactus.php">Contact Us</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+	<div class="slide">
+		<img src="images/banner/contact.jpg" alt="" title="" class="img-responsive" />
+	</div>
+
+	<div class="sam">
+		<div class="container">
+			<h1>Contact Us</h1>
+			<div class="row abt">
+				<div class="col-md-4 ">
+					<div class="contact-address">
+						<ul>
+
+
+							<li><i class="fa  fa-map-marker" aria-hidden="true"></i> <span><b
+										style="color:#7dc0c5; font-size:20px;">Talaviya Impex
+									</b>
+
+									<!-- <p style="margin-left: 61px;"><b style="color:#7dc0c5; font-size:20px;"> Best EXPORTER</b> </p> -->
+
+									<p style="margin-left: 61px;"> 124,Crystal heaven,</p>
+
+									<p style="margin-left: 61px;">
+										Kankot road, mavdi ,Rajkot, India.</p>
+																		
+									<p style="margin-left: 61px;">
+										Pin code : 360005 </p>
+
+
+								</span></li>
+
+							<li><i class="fa fa-phone fa-mobile"  aria-hidden="true"></i> <span><a href="tel:9662514201">+91 96625 14201</a></span>
+								<span><a href="tel:98988 09119">+91 98988 09119</a></span>
+							</li>
+
+							<li style="margin-bottom:0px"><i class="fa fa-phone fa-envelope" aria-hidden="true"></i>
+								<span><a href="mailto:talaviyaimpex5@gmail.com"> talaviyaimpex5@gmail.com</a> </span>
+							</li>
+
+
+
+
+
+						</ul>
+					</div>
+				</div><br>
+
+
+				
+
+				<div class="col-md-8" style="margin-top: 20px;">
+
+					<div class="col-md-6 col-sm-6 ">
+						
+						<form action="" class="webform-client-form" id="contact-form" enctype="multipart/form-data"
+						 onsubmit="return validateForm()"
+							method="post" accept-charset="UTF-8">
+							<label>Name <span class="form-required">*</span>
+							</label>
+							<input type="text" id="edit-submitted-name" name="name" value="" size="60" maxlength="128"
+								class="form-text ">
+
+							<label>Email <span class="form-required">*</span>
+							</label>
+							<input class="email form-text form-email" type="email" id="edit-submitted-email"
+								name="email" size="60">
+
+							<label>Phone/Mobile <span class="form-required">*</span>
+							</label>
+							<input type="text" name="phone" id="phone" value="" size="60" maxlength="128"
+								class="form-text">
+					</div>
+					<div class="col-md-6 col-sm-6 ">
+						<label>Message </label>
+						<textarea id="edit-submitted-message" name="message" cols="60" rows="4"
+							class="form-textarea"></textarea>
+
+						<button type="submit" name="submit" id="edit-submit" value="Submit" 
+							class="more form-submit">Submit</button>
+						</form>
+					</div>
+
+
+
+				</div>
+
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3692.593200432389!2d70.77266021495393!3d22.255508385346843!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjLCsDE1JzE5LjgiTiA3MMKwNDYnMjkuNSJF!5e0!3m2!1sen!2sin!4v1610278575068!5m2!1sen!2sin" width="100%" height="380" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+				<!-- <iframe src="https://goo.gl/maps/KzB8GNodQ4TzGWDB8" width="100%"
+					height="380"></iframe> -->
+
+			</div>
+		</div>
+	</div>
+
+	
+	<div class="final">
+		<div class="container">
+			<div class="pull-left">
+				<p><span class="fine">Copyrights @ 2020 <strong style="color:#7dc0c5;"> Talaviya Impex.</span></strong>
+					All rights Reserved.</p>
+				<p class="sim1">Powered By <a href="#" target="_blank"
+						title="web design company in India">Likezone Infotech Designers</a></p>
+			</div>
+			<div class="pull-right">
+				<p class="sim">Powered By <a href="#" target="_blank"
+						title="web design company in India">Likezone Infotech </a></p>
+			</div>
+		</div>
+	</div>
+	<a href="https://api.whatsapp.com/send?phone=919662514201&text=Hello!%21" class="float" target="_blank">
+		<i class="fa fa-whatsapp my-float"></i>
+		</a> 
+	<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+	<script type="text/javascript" src="js/jquery-1.8.0.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.backtotop.js"></script>
+	<script type="text/javascript" src="js/menuscript.js"></script>
+	<script type="text/javascript">
+		$(function () {
+			if ($.browser.msie && $.browser.version.substr(0, 1) < 7) {
+				$('li').has('ul').mouseover(function () {
+					$(this).children('ul').css('visibility', 'visible');
+				}).mouseout(function () {
+					$(this).children('ul').css('visibility', 'hidden');
+				})
+			}
+
+			/* Mobile */
+			$('#menu-wrap').prepend('<div id="menu-trigger">Menu</div>');
+			$("#menu-trigger").on("click", function () {
+				$("#menu").slideToggle();
+			});
+
+			// iPad
+			var isiPad = navigator.userAgent.match(/iPad/i) != null;
+			if (isiPad) $('#menu ul').addClass('no-transition');
+		});          
+	</script>
+	<script type="text/javascript">
+		
+		function validateForm() {
+			var x = document.forms["contact-form"]["name"].value;
+			if (x == null || x == "") {
+				alert("Name must be filled out");
+				return false;
+			}
+
+			var x = document.forms["contact-form"]["email"].value;
+			var atpos = x.indexOf("@");
+			var dotpos = x.lastIndexOf(".");
+			if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) {
+				alert("Not a valid e-mail address");
+				return false;
+			}
+			var x = document.forms["contact-form"]["phone"].value;
+			if (x == null || x == "") {
+				alert("Phone Number must be filled out");
+				return false;
+			}
+			return confirm("Form Submitted!..");	
+		}
+
+
+		$(document).ready(function () {
+			//called when key is pressed in textbox
+			$("#phone").keypress(function (e) {
+				//if the letter is not digit then display error and don't type anything
+				if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+					//display error message
+					$("#errmsg").html("Digits Only").show().fadeOut("slow");
+					return false;
+				}
+			});
+		});
+
+	</script>
+	<script type="text/javascript">
+		function googleTranslateElementInit() {
+			new google.translate.TranslateElement({ pageLanguage: 'en', includedLanguages: 'en,fr', layout: google.translate.TranslateElement.InlineLayout.SIMPLE }, 'google_translate_element');
+		}
+	</script>
+	</script>
+	<script type="text/javascript"
+		src="../translate.google.com/translate_a/elementa0d8.html?cb=googleTranslateElementInit"></script>
+	<script>
+		(function (i, s, o, g, r, a, m) {
+			i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+				(i[r].q = i[r].q || []).push(arguments)
+			}, i[r].l = 1 * new Date(); a = s.createElement(o),
+				m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+		})(window, document, 'script', '../www.google-analytics.com/analytics.js', 'ga');
+
+		ga('create', 'UA-96765476-1', 'talaviyaimpex.com');
+		ga('send', 'pageview');
+
+	</script>
+
+	<script type="text/javascript">
+		$(document).ready(function () {
+
+
+			$(".valueget").keyup(function () {
+				var dInput = this.value;
+				$.ajax({
+					type: 'GET',
+					url: "valid.php",
+					cache: false,
+					data: { dInput: dInput },
+					success: function (html) {
+
+						if (html == 'success') {
+							$('#err').html('');
+							$(':input[type="submit"]').prop('disabled', false);
+						} else {
+							$('#err').html('Incorrect Captcha');
+							$(':input[type="submit"]').prop('disabled', true);
+						}
+					}
+				});
+
+			});
+
+			$("#new").click(function () {
+				$("#captchaimg").attr("src", "captchad41d.html?" + Math.random());
+			});
+
+		});
+	</script>
+
+
+
+
+	
+</body>
+
+<!-- Mirrored from www.kankasexports.com/import-and-export-business-in-india.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 29 Dec 2020 08:58:22 GMT -->
+
+</html>
